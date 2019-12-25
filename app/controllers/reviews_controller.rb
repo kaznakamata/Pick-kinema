eeb94c9d528d092c322f5a6f7ecf5016958100d6
kaznakamata <kazyokoyama8754@gmail.com>
2019-12-25 20:44:59 +0900

    post review with  body and tmdb

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -4,5 +4,13 @@ class ReviewsController < ApplicationController
   def new
   end
   def create
+    Review.create(review_params)
+    binding.pry
+    redirect_to root_path
+  end
+
+  private
+  def review_params
+    params.require(:review).permit(:tmdb_id, :body)
   end
 end
