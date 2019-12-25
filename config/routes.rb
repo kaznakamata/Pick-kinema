909ca3f9a20ba5a38e06f99598a76da12521af4b
kaznakamata <kazyokoyama8754@gmail.com>
2019-12-25 20:21:37 +0900

    create route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root to: 'reviews#index' 
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :reviews, only: [:show, :new, :create]
 end
